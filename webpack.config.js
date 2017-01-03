8ca296d7881bc0e8c5d924d48113a757a84eb953
hellomephllw <hellomephllw@163.com>
2017-01-03 21:43:53 +0800

    upgrade script loader

--- webpack.config.js
+++ webpack.config.js
@@ -24,10 +24,7 @@ module.exports = {
             {
                 test: /\.js[x]?$/,
                 exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015', 'react']
-                }
+                loader: 'babel-loader'
             },
             //image加载器
             {
